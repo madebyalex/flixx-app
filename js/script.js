3dda4880385b0b4d6bff28c7e28cca41bb0f7907
Alexander Nuzhnyi <hello@alexander.works>
2024-05-10 02:16:17 +0200

    Add: Display popular TV shows

--- js/script.js
+++ js/script.js
@@ -2,29 +2,30 @@ const global = {
   currentPage: window.location.pathname,
 };
 
-async function displayPopularMovies() {
+// Display the most popular movies
+async function displayPopularMovies(num = 8) {
   const { results } = await fetchAPIData('movie/popular');
 
   console.log(results);
 
-  results.forEach((movie) => {
+  results.slice(0, num).forEach((show) => {
     const div = document.createElement('div');
 
     div.classList.add('card');
     div.innerHTML = `
-      <a href="movie-details.html?id=${movie.id}">
+      <a href="tv-details.html?id=${show.id}">
         ${
-          movie.poster_path
+          show.poster_path
             ? `
             <img
-              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
               class="card-img-top"
-              alt="${movie.title}"
+              alt="${show.title}"
             />`
             : `<img
               src="../images/no-image.jpg"
               class="card-img-top"
-              alt="${movie.title}"
+              alt="${show.title}"
             />
             `
         }
@@ -41,6 +42,45 @@ async function displayPopularMovies() {
   });
 }
 
+// Display the most popular shows
+async function displayPopularShows(num = 8) {
+  const { results } = await fetchAPIData('tv/popular');
+
+  console.log(results);
+
+  results.slice(0, num).forEach((show) => {
+    const div = document.createElement('div');
+
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="movie-details.html?id=${show.id}">
+        ${
+          show.poster_path
+            ? `
+            <img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.title}"
+            />`
+            : `<img
+              src="../images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${show.name}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${show.first_air_date}</small>
+        </p>
+      </div>
+    `;
+
+    document.getElementById('popular-shows').appendChild(div);
+  });
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   // const API_KEY = process.env.TMDB_API_KEY;
@@ -48,16 +88,26 @@ async function fetchAPIData(endpoint) {
   const API_KEY = '51353c622a80940c3340c7c92e033e93';
   const API_URL = 'https://api.themoviedb.org/3/';
 
+  showSpinner();
+
   const response = await fetch(
-    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US&page=1`
   );
 
   const data = await response.json();
 
+  hideSpinner();
+
   return data;
 }
 
-fetchAPIData('/');
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
+}
 
 // Highlight active link
 function highlightActiveLink() {
@@ -76,11 +126,12 @@ function init() {
     case '/':
     case '/index.html':
       console.log('Home');
-      displayPopularMovies();
+      displayPopularMovies(12);
       break;
 
     case '/shows.html':
       console.log('Shows');
+      displayPopularShows(12);
       break;
 
     case '/movie-details.html':
